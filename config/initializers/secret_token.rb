ade4fd79bcaf4cf82e46a5d0b9076a307296e6c5
Dan Bickford <danbickford007@yahoo.com>
2013-05-24 00:40:51 -0400

    editor working, several bugs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TextPair::Application.config.secret_token = '49812b8b32110c69fb2932e91212b888d9f8ea769dbec3280af3151f064f45f6b32674646413586b74f08b029c643c16d56114805746aa2317688b090fc9a42c'
